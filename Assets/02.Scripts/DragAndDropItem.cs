ad2e873ef644cf39630eed1e021440998df67e2c
hyeonnnnn <paige4157@gmail.com>
2025-01-06 17:26:23 +0900

    feature: Render Dragged Item First

--- Assets/02.Scripts/DragAndDropItem.cs
+++ Assets/02.Scripts/DragAndDropItem.cs
@@ -9,21 +9,36 @@ public class DragAndDropItem : MonoBehaviour
     [SerializeField] BungeoppangController bungeoppangController;
     [SerializeField] MainCat catController;
 
+    [SerializeField] Camera frontCamera;
     private Camera mainCamera;
+
     private Vector2 startPosition;
+
+    int originalLayerIndex;
+    int draggedItemLayerIndex;
 
     void Start()
     {
         mainCamera = Camera.main;
+
+        // 레이어 정보 저장
+        originalLayerIndex = this.gameObject.layer;
+        draggedItemLayerIndex = LayerMask.NameToLayer("DraggedItem");
     }
 
     void Update()
     {
-        PickUpItem();
-        PutDownItem();
+        if (draggableItem == null)
+        {
+            PickUpItem();
+        }
+        else
+        {
+            PutDownItem();
+        }
     }
 
-    // ������ ����
+    // 아이템 집기
     void PickUpItem()
     {
         if (Input.GetMouseButtonDown(0))
@@ -37,7 +52,8 @@ public class DragAndDropItem : MonoBehaviour
 
                 if (draggableItem != null)
                 {
-                    Debug.Log("������ ����");
+                    // 레이어 바꿔서 화면 제일 위에 렌더링하기
+                    draggableItem.StartDragging(draggedItemLayerIndex, frontCamera);
                     startPosition = draggableItem.transform.position;
                     draggableItem.isDrag = true;
                 }
@@ -45,71 +61,77 @@ public class DragAndDropItem : MonoBehaviour
         }
     }
 
-    // ������ ����
+    // 아이템 놓기
     void PutDownItem()
     {
         if (Input.GetMouseButtonUp(0))
         {
+            // 원래 레이어로 바꾸기
+            draggableItem.EndDragging(originalLayerIndex);
+
             if (draggableItem != null)
             {
-                // ��Ḧ �ͼ��⿡ ����
-                if (mixerController != null && draggableItem.CompareTag("Ingredient") && IsOverlapping(draggableItem.gameObject, mixerController.gameObject))
-                {
-                    mixerController.CheckComponent(draggableItem.gameObject);
-                }
+                HandleDrop();
 
-                // ������ ���쿡 ����
-                else if (ovenController != null && draggableItem.CompareTag("Dough") && IsOverlapping(draggableItem.gameObject, ovenController.gameObject))
-                {
-                    ovenController.CheckComponent(draggableItem.gameObject);
-                }
+                // 아이템 원래 자리로 돌아가기
+                draggableItem.transform.position = startPosition;
+                draggableItem.isDrag = false;
+                draggableItem = null;
+            }
+        }
+    }
 
-                // �ʸ��� �ؾ�� ���
-                else if (draggableItem.CompareTag("Filling"))
-                {
-                    if (bungeoppangController.bungeoppang1 != null && IsOverlapping(draggableItem.gameObject, bungeoppangController.bungeoppang1.gameObject))
-                    {
-                        bungeoppangController.FillBungeobbang(bungeoppangController.bungeoppang1.gameObject, draggableItem.gameObject);
-                    }
-                    else if (bungeoppangController.bungeoppang2 != null && IsOverlapping(draggableItem.gameObject, bungeoppangController.bungeoppang2.gameObject))
-                    {
-                        bungeoppangController.FillBungeobbang(bungeoppangController.bungeoppang2.gameObject, draggableItem.gameObject);
-                    }
-                    else if (bungeoppangController.bungeoppang3 != null && IsOverlapping(draggableItem.gameObject, bungeoppangController.bungeoppang3.gameObject))
-                    {
-                        bungeoppangController.FillBungeobbang(bungeoppangController.bungeoppang3.gameObject, draggableItem.gameObject);
-                    }
-                }
-                
-                else if (draggableItem.CompareTag("Cat"))
-                {
-                    if (catController != null)
-                    {
-                        Components components = draggableItem.GetComponent<Components>();
-                        if (components != null && components.componentData != null)
-                        {
-                            catController.ReceiveBungeobbangData(components.componentData);
-                            Debug.Log($"고양이에게 아이템 전달: {components.componentData.componentName}");
-                        }
-                        else
-                        {
-                            Debug.LogWarning("아이템에 유효한 데이터가 없습니다.");
-                        }
-                    }
-                }
+    // 드랍 로직
+    void HandleDrop()
+    {
+        // 재료를 믹서기에 넣기
+        if (mixerController != null && draggableItem.CompareTag("Ingredient") && IsOverlapping(draggableItem.gameObject, mixerController.gameObject))
+        {
+            mixerController.CheckComponent(draggableItem.gameObject);
+        }
 
-                // �巡�� �� ���� ��ġ��
-                if (draggableItem != null)
+        // 밀가루를 오븐에 넣기
+        else if (ovenController != null && draggableItem.CompareTag("Dough") && IsOverlapping(draggableItem.gameObject, ovenController.gameObject))
+        {
+            ovenController.CheckComponent(draggableItem.gameObject);
+        }
+
+        // 필링을 빈 붕어빵에 넣기
+        else if (draggableItem.CompareTag("Filling"))
+        {
+            if (bungeoppangController.bungeoppang1 != null && IsOverlapping(draggableItem.gameObject, bungeoppangController.bungeoppang1.gameObject))
+            {
+                bungeoppangController.FillBungeobbang(bungeoppangController.bungeoppang1.gameObject, draggableItem.gameObject);
+            }
+            else if (bungeoppangController.bungeoppang2 != null && IsOverlapping(draggableItem.gameObject, bungeoppangController.bungeoppang2.gameObject))
+            {
+                bungeoppangController.FillBungeobbang(bungeoppangController.bungeoppang2.gameObject, draggableItem.gameObject);
+            }
+            else if (bungeoppangController.bungeoppang3 != null && IsOverlapping(draggableItem.gameObject, bungeoppangController.bungeoppang3.gameObject))
+            {
+                bungeoppangController.FillBungeobbang(bungeoppangController.bungeoppang3.gameObject, draggableItem.gameObject);
+            }
+        }
+
+        else if (draggableItem.CompareTag("Cat"))
+        {
+            if (catController != null)
+            {
+                Components components = draggableItem.GetComponent<Components>();
+                if (components != null && components.componentData != null)
                 {
-                    draggableItem.transform.position = startPosition;
-                    draggableItem.isDrag = false;
-                    draggableItem = null;
+                    catController.ReceiveBungeobbangData(components.componentData);
+                    Debug.Log($"고양이에게 아이템 전달: {components.componentData.componentName}");
+                }
+                else
+                {
+                    Debug.LogWarning("아이템에 유효한 데이터가 없습니다.");
                 }
             }
         }
     }
 
-    // �����ִ��� Ȯ���ϱ�
+    // 닿아 있는지 확인하기
     private bool IsOverlapping(GameObject obj1, GameObject obj2)
     {
 
--- Assets/02.Scripts/DraggableItem.cs
+++ Assets/02.Scripts/DraggableItem.cs
@@ -5,13 +5,36 @@ public class DraggableItem : MonoBehaviour
     public Components ingredientData; // ScriptableObject 연결
     public bool isDrag = false;
 
+    [SerializeField] private Camera frontCamera;
+
+    private int originalLayerIndex;
+    private int draggedItemLayerIndex;
+
     void Update()
     {
-        if (isDrag)
+        if (isDrag && frontCamera != null)
         {
-            Vector2 currentPosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            Vector2 currentPosition = frontCamera.ScreenToWorldPoint(Input.mousePosition);
             transform.position = currentPosition;
         }
+    }
+
+    // 드래그 시작
+    public void StartDragging(int newLayer, Camera camera)
+    {
+        originalLayerIndex = gameObject.layer;
+        draggedItemLayerIndex = newLayer;
+        gameObject.layer = draggedItemLayerIndex;
+        frontCamera = camera;
+        isDrag = true;
+    }
+
+    // 드래그 종료
+    public void EndDragging(int restoreLayer)
+    {
+        gameObject.layer = restoreLayer;
+        isDrag = false;
+        frontCamera = null;
     }
 }
 
