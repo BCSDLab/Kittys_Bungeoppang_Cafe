0a36b75673e7a402b473ecc4e8db83ab425f31a9
hyeonnnnn <paige4157@gmail.com>
2025-01-07 16:13:43 +0900

    bound fix

--- Assets/02.Scripts/DragAndDropItem.cs
+++ Assets/02.Scripts/DragAndDropItem.cs
@@ -119,7 +119,7 @@ public class DragAndDropItem : MonoBehaviour
         
         if (draggableItem.CompareTag("Bungeobbang"))
         {
-            if(!IsOverlapping(draggableItem.gameObject, bound))
+            if(IsOverlapping(draggableItem.gameObject, bound))
             {
                 catController.CheckComponent(draggableItem.gameObject);
                 Destroy(draggableItem.gameObject);
