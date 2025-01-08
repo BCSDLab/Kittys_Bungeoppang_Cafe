07a59b73b0817f157009ee4b14389cbc6008181a
hyeonnnnn <paige4157@gmail.com>
2025-01-08 09:19:25 +0900

    fix: Game Retry

--- Assets/02.Scripts/Exit.cs
+++ Assets/02.Scripts/Exit.cs
@@ -4,7 +4,10 @@ public class Exit : MonoBehaviour
 {
     public void ExitGame()
     {
+#if UNITY_EDITOR
         UnityEditor.EditorApplication.isPlaying = false;
+#else
         Application.Quit();
+#endif
     }
 }
--- Assets/02.Scripts/GameControl.cs
+++ Assets/02.Scripts/GameControl.cs
@@ -5,8 +5,20 @@ public class GameControl : MonoBehaviour
 {
     [SerializeField] GameObject guidePanel;
     [SerializeField] Text textBGM;
+    [SerializeField] GameObject cookingSystem;
+    [SerializeField] GameObject cookingTool;
+    [SerializeField] GameObject cat;
+
     static bool isFirst = true;
     bool isBGMPlaying = true;
+
+    void Awake()
+    {
+    #if UNITY_STANDALONE
+        Screen.SetResolution(540, 960, false);
+        Screen.fullScreen = false;
+    #endif
+    }
 
     void Start()
     {
@@ -17,6 +29,22 @@ public class GameControl : MonoBehaviour
                 guidePanel.SetActive(true);
                 isFirst = false;
             }
+
+            if(cookingSystem!=null && cookingTool != null && cat != null)
+            {
+                cookingSystem.SetActive(false);
+                cookingTool.SetActive(false);
+                cat.SetActive(false);
+            }
+        }
+        else
+        {
+            if (cookingSystem != null && cookingTool != null && cat != null)
+            {
+                cookingSystem.SetActive(true);
+                cookingTool.SetActive(true);
+                cat.SetActive(true);
+            }
         }
     }
 
@@ -26,13 +54,13 @@ public class GameControl : MonoBehaviour
         {
             AudioManager.audioManager.StopBGM();
             isBGMPlaying = false;
-            textBGM.text = "BGM ÄÑ±â";
+            textBGM.text = "BGM On";
         }
         else
         {
             AudioManager.audioManager.PlayBGM("PlayBGM");
             isBGMPlaying = true;
-            textBGM.text = "BGM ²ô±â";
+            textBGM.text = "BGM Off";
         }
     }
 
